2e565d85384c364fc3087d4ccef56fb6780e22ea
Ross Kirkpatrick <ross.kirkpatrick@suse.com>
2022-05-25 13:05:12 -0400

    init of base resources

--- setup.ps1
+++ setup.ps1
@@ -1,4 +1,5 @@
-Invoke-WebRequest -Uri https://aka.ms/installazurecliwindows -OutFile .\AzureCLI.msi; Start-Process msiexec.exe -Wait -ArgumentList '/I AzureCLI.msi /quiet'; rm .\AzureCLI.msi
+$ProgressPreference='SilentlyContinue'
+Invoke-WebRequest -Uri https://aka.ms/installazurecliwindows -OutFile .\AzureCLI.msi; Start-Process msiexec.exe -Wait -ArgumentList '/I AzureCLI.msi /quiet'; Remove-Item .\AzureCLI.msi
 
 az login
 
